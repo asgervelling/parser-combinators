1feff174f43e4f9ecf7d3966f42a601b67cac95c
a <a@pop-os.localdomain>
2023-12-12 12:14:45 +0100

    either

--- src/combinators.ts
+++ src/combinators.ts
@@ -1,5 +1,8 @@
 type Combinator = (s: string) => boolean;
 
+/**
+ * True if the first char of the string matches c.
+ */
 export function char(c: string): Combinator {
   return (s: string) => {
     if (s[0] === c) {
@@ -8,5 +11,20 @@ export function char(c: string): Combinator {
     else {
       return false;
     }
-  }
+  };
+}
+
+/**
+ * True if either of the combinators returns true.
+ */
+export function either(...combinators: Combinator[]) {
+  return (s: string) => {
+    for (let i = 0; i < combinators.length; i++) {
+      if (combinators[i](s)) {
+        return true;
+      }
+    }
+
+    return false;
+  };
 }
--- tests/char.test.ts
+++ tests/char.test.ts
@@ -1,4 +1,4 @@
-import { char } from "../src/combinators";
+import { char, either } from "../src/combinators";
 
 describe("char", () => {
   test("matches a single character", () => {
@@ -7,5 +7,18 @@ describe("char", () => {
 
   test("fails if the character is not expected", () => {
     expect(char("e")("Hello")).toBe(false);
+  });
+});
+
+describe("either", () => {
+  const aOrB = either(char("a"), char("b"));
+
+  test("returns true if any of the combinators return true", () => {
+    expect(aOrB("a")).toBe(true);
+    expect(aOrB("b")).toBe(true);
+  });
+
+  test("returns false if any of the combinators return false", () => {
+    expect(aOrB("c")).toBe(false);
   });
 });
