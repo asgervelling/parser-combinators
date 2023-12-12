4aad17ddbbed29f8b82fe8b956ce8e134d0c62f8
a <a@pop-os.localdomain>
2023-12-12 12:08:55 +0100

    Simple test case

--- tests/char.test.ts
+++ tests/char.test.ts
@@ -1,2 +1,11 @@
-import { char } from "./combinators";
+import { char } from "../src/combinators";
 
+describe("char", () => {
+  test("matches a single character", () => {
+    expect(char("H")("Hello")).toBe(true);
+  });
+
+  test("fails if the character is not expected", () => {
+    expect(char("e")("Hello")).toBe(false);
+  });
+});
